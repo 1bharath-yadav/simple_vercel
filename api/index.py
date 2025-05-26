543d89d51eccf2024e9fbf821408156a94f4ec51
1bharath-yadav <byadhav36@gmail.com>
2025-05-26 16:11:57 +0530

    feat: enhance FastAPI application with CORS middleware and job management endpoints

--- api/index.py
+++ api/index.py
@@ -1,6 +1,77 @@
-from fastapi import FastAPI, Request, Response
-app = FastAPI()
-@app.get("/")
-def index():
-    return {"message": "Hello World"}
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI ()
+
+app.add_middleware (
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get ("/")
+def index ():
+    return {
+        "messsage": "Welcome to the FastAPI application!",
+    }
+
+@app.get ("/api/params")
+def search (request: Request):
+    parameters = list ()
+    parameter_values = list ()
+
+    for parameter_name in request.query_params.keys ():
+        
+        parameter_values = request.query_params.getlist (parameter_name)
+        for value in parameter_values:
+            parameters.append ({
+                "name": parameter_name,
+                "value": value
+            })
+
+
+    print (parameters)
+
+    return {
+        "parameters": parameters,
+    }
+
+from pydantic import BaseModel
+
+
+class Job (BaseModel):
+    name: str
+    cost: int
+
+jobs = list ()
+
+@app.post ("/api/create")
+def create (job: Job):
+    jobs.append (job)
+
+    return {
+        "job": job
+    }
+
+@app.get ("/api/jobs")
+def get_jobs ():
+    return {
+        "jobs": jobs
+    }
+
+@app.delete ("/api/delete/{job_no}")
+def delelte_job (job_no: int):
+    if job_no < 0 or job_no >= len (jobs):
+        return {
+            "error": "Job not found"
+        }
+
+    job = jobs.pop (job_no)
+
+    return {
+        "job": job
+    }
+
 
